1b2cf37d9a80a7fba2743a97289743ee08142ff8
Yan Liu <yanliuse7@gmail.com>
2022-03-11 14:10:35 -0500

    Add delete task function.

--- task-manager/src/components/ListingTask/ListingTask.js
+++ task-manager/src/components/ListingTask/ListingTask.js
@@ -6,39 +6,58 @@ class ListingTask extends Component {
     categoryList: []
   }
 
+  sortTasksByCategory(data) {
+    const categoryList = [];
+    data.forEach((task) => {
+      const currentCategory = task.category;
+      let categoryExsited = false;
+      for (let i = 0; i < categoryList.length; ++i) {
+        if (categoryList[i].categoryName === currentCategory) {
+          categoryList[i].tasks.push(task);
+          categoryExsited = true;
+          break;
+        }
+      }
+      if (!categoryExsited) {
+        const newCategory = {
+          categoryName: currentCategory,
+          tasks: [task]
+        };
+        categoryList.push(newCategory);
+      }
+    })
+    this.setState({ categoryList });
+  }
+
   componentDidMount() {
     axios.get('http://localhost:56789/tasks')
-      .then((res) => {
-        const categoryList = [];
-        res.data.forEach((task) => {
-          const currentCategory = task.category;
-          let categoryExsited = false;
-          for (let i = 0; i < categoryList.length; ++i) {
-            if (categoryList[i].categoryName === currentCategory) {
-              categoryList[i].tasks.push(task);
-              categoryExsited = true;
-              break;
-            }
-          }
-          if (!categoryExsited) {
-            const newCategory = {
-              categoryName: currentCategory,
-              tasks: [task]
-            };
-            categoryList.push(newCategory);
-          }
-        })
-        this.setState({ categoryList });
+      .then((response) => {
+        this.sortTasksByCategory(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   }
 
+  handleDelete = (event) => {
+    event.preventDefault();
+    const taskDiv = event.target.closest(".task");
+    for (let i = 0; i < taskDiv.attributes.length; ++i) {
+      const { name, value } = taskDiv.attributes[i];
+      if (name === "id") {
+        axios.delete(`http://localhost:56789/tasks/${value}`)
+          .then((response) => {
+            this.sortTasksByCategory(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }
+  }
+
   render() {
     const { categoryList } = this.state;
-    console.log(categoryList.length);
-
     return (
       categoryList.length === 0 ?
       <></> :
@@ -46,14 +65,21 @@ class ListingTask extends Component {
         {
           categoryList.map((category) => {
             return (
-              <div className="tasks__container">
+              <div
+                className="tasks__container"
+                key={category.categoryName}
+              >
                 <h2 className="tasks__category">
                   {category.categoryName}
                 </h2>
                 {
                   category.tasks.map((task) => {
                     return (
-                      <div className="task">
+                      <div
+                        className="task"
+                        id={task.id}
+                        key={task.id}
+                      >
                         <p className="task__content">
                           {task.content}
                         </p>
@@ -63,7 +89,10 @@ class ListingTask extends Component {
                         <p className="task__date">
                           {new Date(task.due).toLocaleDateString('en-US')}
                         </p>
-                        <button className="task__btton">
+                        <button
+                          className="task__button"
+                          onClick={this.handleDelete}
+                        >
                           Delete
                         </button>
                       </div>
