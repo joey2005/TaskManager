df8f3f033a8cbd4592b05bcccf778b4308293ef0
angrajlatake <angraj.latake@gmail.com>
2022-03-11 12:55:33 -0500

    added state and trying to render dynamic components

--- task-manager/src/components/ListingTask/ListingTask.js
+++ task-manager/src/components/ListingTask/ListingTask.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
 import "./ListingTask.scss";
-
+import axios from 'axios';
 class ListingTask extends Component {
+  state = {
+    task: [],
+    category: []
+  }
+  componentDidMount(){
+    axios
+    .get('http://localhost:56789/tasks')
+    .then(res => {
+      const categoryList = [];
+      res.data.forEach(element => {
+        if (!categoryList.find(category =>{
+          return category===element.category
+        })){ categoryList.push(element.category);
+           const index = categoryList.indexOf(category =>{
+            return category===element.category
+          }
+           )
+      });
+      console.log(res)}
+    )}
   render() {
     return (
+      this.state.task.length === 0 ? <div></div> :
       <>
         <section className="tasks">
           <div className="tasks__container">
